878c391dabd136ee99920c0e28469d390e7eb55a
m4nos <herman226.r@gmail.com>
2020-09-28 18:39:54 +0300

    check user inputs for registration

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const { UserInputError } = require("apollo-server");
+
 const { User } = require("../models");
 
 module.exports = {
@@ -12,6 +15,51 @@ module.exports = {
       }
 
       return users;
+    },
+  },
+  Mutation: {
+    register: async (_, args) => {
+      let { username, email, password, confirmPassword } = args;
+      let errors = {};
+      try {
+        if (email.trim() === "") errors.email = "Email must not be empty";
+        if (username.trim() === "")
+          errors.username = "Username must not be empty";
+        if (password.trim() === "")
+          errors.password = "Password must not be empty";
+        if (confirmPassword.trim() === "")
+          errors.confirmPassword = "Password must not be empty";
+
+        if (password !== confirmPassword)
+          errors.confirmPassword = "passwords must be the same";
+
+        // const userByUsername = await User.findOne({ where: { username } });
+        // const userByEmail = await User.findOne({ where: { email } });
+
+        // if (userByUsername) errors.username = "Username is not available";
+        // if (userByEmail) errors.email = "Email is not available";
+
+        if (Object.keys(errors).length > 0) throw errors;
+
+        password = await bcrypt.hash(password, 6);
+
+        const user = await User.create({
+          username,
+          email,
+          password,
+        });
+        return user;
+      } catch (err) {
+        console.log(err);
+        if (err.name === "SequelizeUniqueConstraintError") {
+          err.errors.forEach(
+            (e) => (errors[e.path] = `${e.path} is already taken`)
+          );
+        } else if (err.name === "SequelizeValidationError") {
+          err.errors.forEach((e) => (errors[e.path] = e.message));
+        }
+        throw new UserInputError("Bad input", { errors });
+      }
     },
   },
 };
