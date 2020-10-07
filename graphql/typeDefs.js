c6ffc904641b8330674d4cf6fa17cf1eb7415d22
m4nos <herman226.r@gmail.com>
2020-10-07 14:10:03 +0300

    refactor resolvers/add message model

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,9 +6,17 @@ module.exports = gql`
     email: String!
     token: String
   }
+  type Message {
+    uuid: String!
+    content: String!
+    from: String!
+    to: String!
+    createdAt: String!
+  }
   type Query {
     getUsers: [User]!
     login(username: String!, password: String!): User!
+    getMessages(from: String!): [Message]!
   }
   type Mutation {
     register(
@@ -17,5 +25,6 @@ module.exports = gql`
       password: String!
       confirmPassword: String!
     ): User!
+    sendMessage(to: String!, content: String!): Message!
   }
 `;
